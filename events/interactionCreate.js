966e25684fabdb5a3129ff3fc927daad8a709d29
totktoizniotkuda <43038535+totktoizniotkuda@users.noreply.github.com>
2022-04-16 22:42:45 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -39,7 +39,7 @@ module.exports = {
           .setColor('6d6ee8')
           .setAuthor({name: `${interaction.user.username}'s Ticket`, iconURL: 'https://i.imgur.com/oO5ZSRK.png'})
           .setDescription('Select the category of your ticket')
-          .setFooter({text: `${client.user.tag} || github.com/blackknight683`, iconURL: client.user.displayAvatarURL()})
+          .setFooter({text: `${client.user.tag} || github.com/totktoizniotkuda`, iconURL: client.user.displayAvatarURL()})
           .setTimestamp();
 
         const row = new client.discord.MessageActionRow()
@@ -84,7 +84,7 @@ module.exports = {
                   .setColor('6d6ee8')
                   .setAuthor({name: 'Ticket', iconURL: interaction.user.displayAvatarURL()})
                   .setDescription(`<@!${interaction.user.id}> Created a ticket with issues regarding \`${i.values[0]}\``)
-                  .setFooter({text: `${client.user.tag} || github.com/blackknight683`, iconURL: client.user.displayAvatarURL()})
+                  .setFooter({text: `${client.user.tag} || github.com/totktoizniotkuda`, iconURL: client.user.displayAvatarURL()})
                   .setTimestamp();
 
                 const row = new client.discord.MessageActionRow()
